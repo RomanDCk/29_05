47884146c9b3c3cc34a0a41b12bc715a75cf27e1
roman <romanmartsiv2613>
2023-06-11 17:01:30 +0300

    first commit

---
+++ dz4.py
@@ -0,0 +1,11 @@
+a = input('sing(+, -, *, /):')
+b = float(input('b = '))
+c = float(input('c = '))
+if a == '+':
+    print(b + c)
+elif a == '-':
+    print(b - c)
+elif a == '*':
+    print(b * c)
+elif a == '/':
+    print(b / c)
