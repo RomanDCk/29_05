ad572ff29ac5abcfb1fbed4b92b54e2a8ea5e841
roman <romanmartsiv2613>
2023-06-11 20:00:14 +0300

    first commit

---
+++ dz5.py
@@ -0,0 +1,46 @@
+lst1 = [1, 2, 3, 4, 5, 6]
+lst_lst = [
+    [],
+    []
+]
+lst_lst[0].insert (0,1)
+lst_lst[0].insert (1,2)
+lst_lst[0].insert (2,3)
+lst_lst[1].insert (0,4)
+lst_lst[1].insert (1,5)
+lst_lst[1].insert (2,6)
+print(lst1)
+print(lst_lst)
+
+lst2 = [1, 2, 3]
+lst_lst2 = [[],[]]
+lst_lst2[0].insert (0,1)
+lst_lst2[0].insert (1,2)
+lst_lst2[1].insert (0,3)
+print(lst2)
+print(lst_lst2)
+
+
+lst3 = [1, 2, 3, 4, 5]
+lst_lst3 = [[],[]]
+lst_lst3[0].insert (0,1)
+lst_lst3[0].insert (1,2)
+lst_lst3[0].insert (2,3)
+
+lst_lst3[1].insert (0,4)
+lst_lst3[1].insert (1,5)
+print(lst3)
+print(lst_lst3)
+
+lst4 = [1]
+lst_lst4 = [[],[]]
+lst_lst4[0].insert (0,1)
+print(lst4)
+print(lst_lst4)
+
+lst5 = []
+lst_lst5 = [[],[]]
+lst_lst[0].insert (0,0)
+
+print(lst5)
+print(lst_lst5)
