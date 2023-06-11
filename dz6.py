8a55a847b2f23dea5030df4136bd3a8a616edc7b
roman <romanmartsiv2613>
2023-06-11 20:42:51 +0300

    first commit

--- dz6.py
+++ dz6.py
@@ -1,1 +1,18 @@
-print('hello world')
+first_list = [12,3,4,10]
+a = first_list.pop()
+first_list.insert(0,10)
+print(first_list)
+
+second_list = [1]
+b = second_list.pop()
+second_list.insert(0,1)
+print(second_list)
+
+third_list = []
+
+print(third_list)
+
+fourth_list = [12, 3, 4, 10, 8]
+c = fourth_list.pop()
+fourth_list.insert(0,8)
+print(fourth_list)
